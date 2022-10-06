52e5c3fa6f6688d83034c8c09b72f5f139156cbd
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-06 23:06:14 +0200

    add logic for attack button

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,3 +3,10 @@ let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
+
+function attackHandler() {
+  const damage = dealMonsterDamage(ATTACK_VALUE);
+  dealPlayerDamage(damage);
+}
+
+attackBtn.addEventListener("click", attackHandler);
