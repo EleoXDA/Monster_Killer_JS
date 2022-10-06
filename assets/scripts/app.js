3493824447ad7f78436aa821c61c18f3103e32b4
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-06 23:18:32 +0200

    simplify standard/strong attack handler functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -13,36 +13,27 @@ function attackMonster(mode){
   } else if (mode === 'STRONG_ATTACK'){
     maxDamage = STRONG_ATTACK_VALUE;
   }
+
+  const damage = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert("You won!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert("You lost!");
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+    alert("You have a draw!");
+  }
 }
 
 function attackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE);
-  currentMonsterHealth -= damage;
-  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-  currentPlayerHealth -= playerDamage;
-
-  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
-    alert("You won!");
-  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-    alert("You lost!");
-  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
-    alert("You have a draw!");
-  }
+  attackMonster('ATTACK');
 }
 
 function strongAttackHandler() {
-  const damage = dealMonsterDamage(STRONG_ATTACK_VALUE);
-  currentMonsterHealth -= damage;
-  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-  currentPlayerHealth -= playerDamage;
-
-  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
-    alert("You won!");
-  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-    alert("You lost!");
-  } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
-    alert("You have a draw!");
-  }
+  attackMonster('STRONG_ATTACK');
 }
 
 
