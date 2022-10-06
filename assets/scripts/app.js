12d51deb2f14707b5d4962c274836b786ad2cc3a
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-06 23:48:59 +0200

    add constants for ease of use in functions

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,6 +3,9 @@ const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 const HEAL_VALUE = 20;
 
+const MODE_ATTACK = 'ATTACK'; // MODE_ATTACK = 0
+const MODE_STRONG_ATTACK = 'STRONG_ATTACK'; // MODE_STRONG_ATTACK = 1
+
 const enteredValue = prompt("What is your max life for you and the monster?", "100");
 
 let chosenMaxLife = parseInt(enteredValue);
@@ -10,6 +13,7 @@ let chosenMaxLife = parseInt(enteredValue);
 if (isNaN(chosenMaxLife) || chosenMaxLife <= 0) {
   chosenMaxLife = 100;
 }
+
 let currentMonsterHealth = chosenMaxLife;
 let currentPlayerHealth = chosenMaxLife;
 let hasBonusLife = true;
