852e656931a4d4c416252addabca011ba1445644
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-06 23:07:27 +0200

    IF condition for winning

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,6 +7,10 @@ adjustHealthBars(chosenMaxLife);
 function attackHandler() {
   const damage = dealMonsterDamage(ATTACK_VALUE);
   dealPlayerDamage(damage);
+
+  if (currentMonsterHealth <= 0) {
+    alert("You won!");
+  }
 }
 
 attackBtn.addEventListener("click", attackHandler);
