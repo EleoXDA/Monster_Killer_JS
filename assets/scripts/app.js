f33e45224e06f39234e6ca8ab5f34769ca662650
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-06 23:23:25 +0200

    add an end_round function that finalizes game

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,21 +1,14 @@
 const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
+const HEAL_VALUE = 20;
 
 let chosenMaxLife = 100;
 let currentMonsterHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
-function attackMonster(mode){
-  if (mode === 'ATTACK'){
-    maxDamage = ATTACK_VALUE;
-  } else if (mode === 'STRONG_ATTACK'){
-    maxDamage = STRONG_ATTACK_VALUE;
-  }
-
-  const damage = dealMonsterDamage(maxDamage);
-  currentMonsterHealth -= damage;
+function endRound() {
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
 
@@ -26,6 +19,18 @@ function attackMonster(mode){
   } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
     alert("You have a draw!");
   }
+}
+
+function attackMonster(mode){
+  if (mode === 'ATTACK'){
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === 'STRONG_ATTACK'){
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+
+  const damage = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= damage;
+  endRound();
 }
 
 function attackHandler() {
