2c75f34cb4e4f9dbd0e83c78dff3c66099e1499d
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-07 20:53:40 +0200

    assignment #4-1

---
+++ assets/scripts/assignment-4.js
@@ -0,0 +1,8 @@
+const randomNumber = Math.random(); // produces random number between 0 (including) and 1 (excluding)
+
+// assignment #1
+if (randomNumber > 0.7) {
+  alert(`${randomNumber} is greater than 0.7`)
+} else {
+  alert(`${randomNumber} is NOT greater than 0.7`)
+}
