0041f3a12979cbd1e6424e0e27b0e9c1b76dfce2
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-07 20:57:58 +0200

    assignment #4-2

--- assets/scripts/assignment-4.js
+++ assets/scripts/assignment-4.js
@@ -6,3 +6,22 @@ if (randomNumber > 0.7) {
 } else {
   alert(`${randomNumber} is NOT greater than 0.7`)
 }
+
+//assignment #2
+// first way
+const arrayOfNumbers = [6,4,7,4,2,1,7,9,3,0.4,-5];
+
+for (let i = 0; i < arrayOfNumbers.length; i++){
+  console.log(arrayOfNumbers[i]);
+}
+// second way
+for (const number of arrayOfNumbers) {
+  console.log(number);
+}
+// third way
+let counter = 0
+while (counter < arrayOfNumbers.length) {
+  console.log(arrayOfNumbers[counter]);
+  counter++
+}
+
