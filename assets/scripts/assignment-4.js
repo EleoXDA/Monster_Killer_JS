c2bd0d2a734d6e8a2ce808b80add9f4a51886553
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-07 21:05:30 +0200

    assignment #4-4

--- assets/scripts/assignment-4.js
+++ assets/scripts/assignment-4.js
@@ -2,16 +2,16 @@ const randomNumber = Math.random(); // produces random number between 0 (includi
 
 // assignment #1
 if (randomNumber > 0.7) {
-  alert(`${randomNumber} is greater than 0.7`)
+  alert(`${randomNumber} is greater than 0.7`);
 } else {
-  alert(`${randomNumber} is NOT greater than 0.7`)
+  alert(`${randomNumber} is NOT greater than 0.7`);
 }
 
 //assignment #2
 // first way
-const arrayOfNumbers = [6,4,7,4,2,1,7,9,3,0.4,-5];
+const arrayOfNumbers = [6, 4, 7, 4, 2, 1, 7, 9, 3, 0.4, -5];
 
-for (let i = 0; i < arrayOfNumbers.length; i++){
+for (let i = 0; i < arrayOfNumbers.length; i++) {
   console.log(arrayOfNumbers[i]);
 }
 // second way
@@ -19,20 +19,33 @@ for (const number of arrayOfNumbers) {
   console.log(number);
 }
 // third way
-let counter = 0
+let counter = 0;
 while (counter < arrayOfNumbers.length) {
   console.log(arrayOfNumbers[counter]);
-  counter++
+  counter++;
 }
 
 //assignment #3
 // first way
-for (let i = arrayOfNumbers.length-1; i < 0; i++){
+for (let i = arrayOfNumbers.length - 1; i < 0; i++) {
   console.log(arrayOfNumbers[i]);
 }
 //second way
-let counter2 = arrayOfNumbers.length
+let counter2 = arrayOfNumbers.length;
 while (counter > 0) {
   console.log(arrayOfNumbers[counter]);
-  counter--
+  counter--;
+}
+
+// assignment #4
+const randomNumber2 = Math.random(); // produces random number between 0 (including) and 1 (excluding)
+
+if (
+  (randomNumber > 0.7 && randomNumber2 > 0.7) ||
+  randomNumber <= 0.2 ||
+  randomNumber2 <= 0.2
+) {
+  alert(`${randomNumber} and ${randomNumber2} are both greater than 0.7 or at least one of them is NOT greater than 0.2`);
+} else {
+  alert(`Either ${randomNumber} or ${randomNumber2} is smaller than 0.7 or both of them is greater than 0.2`);
 }
