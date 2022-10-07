d968db7fb888ea76fc68184d70cabb48b15059d1
Elbay Malik <27622683+EleoXDA@users.noreply.github.com>
2022-10-07 20:16:17 +0200

    add a backwards counting counter with a FOR loop

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -175,6 +175,9 @@ function printLogHandler() {
   for (let i = 0; i < 3; i++){
     console.log('======')
   }
+  for (let i = 10; i > 0; i--) {
+    console.log(i);
+  }
   console.log(battleLog);
 }
 
